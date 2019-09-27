d61eacbaf2ce72637b9ce2d5a7d035de6bfbd61f
Cinthia <ckstgarcia@gmail.com>
2019-09-26 19:01:45 -0500

    get functions was added

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+const port = process.env.PORT || 3003
+
+
+
+let products=[
+  {id:'01', name:'frappe mazapan', price:'30', upgrade:'2019-09'},
+  {id:'02', name:'frapuccino', price:'40', upgrade:'2019-09'},
+  {id:'03', name:'frape moka', price:'35', upgrade:'2019-09'}
+]
+
+app.get('/',(req, res)=> {
+  res.send('Hola desde el backend')
+})
+app.get('/api/products/list',(req, res)=> {
+  res.send(['frape mazapan', 'frapuccino', 'frape moka' ])
+})
+app.get('/api/products',(req, res)=> {
+  res.send(products)
+})
+app.get('/api/products/:name', (req, res)=> {
+  const product= products.find(product=>product.name === req.params.name)
+  if (!product){
+    res.status(404).send('producto no encontrado!')
+  }else{res.send(products)}
+  
+})
+
+app.get('/api/products/:name/:price/:upgrade', (req, res)=> {
+  res.send(req.params)
+})
+
+app.post('/api/products',(req, res)=>{
+  let productId= products.length;
+  let product={
+    id:productId,
+    name: req.params.name,
+    price: req.params.price,
+    upgrade: req.params.upgrade
+  }
+
+  products.push(product);
+  res.send(product);
 })
  
-app.listen(3000, console.log('Escuchando en puerto 3000'))
+app.listen(port, console.log('Escuchando en puerto: '+port))
