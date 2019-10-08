5a9f110cec4e87e0c421fcdbdf7ccc0649fc317b
Cinthia <ckstgarcia@gmail.com>
2019-10-08 17:04:03 -0500

    methods get and post were upgrade

--- routes/products.js
+++ routes/products.js
@@ -10,29 +10,30 @@ const { check, validationResult } = require('express-validator');//libreria de v
     res.json(products)
   })
 
-  router.get('/:id', async (req, resp)=>{
-    const product= await Product.findById (req.params.id)
-  if(!product) return res.status(404).send('No se encontró el producto')
-  res.json(product)
+  router.get('/:id', async (req, res)=>{
+    const product= await Product.findById(req.params.id)
+    if(!product) return res.status(404).send('No se encontró el producto')
+    res.json(product)
   })
  
   router.post('/', [
     check('name').isLength({min:2}),
-    check('price').isLength({min:1})
+    check('price').isLength({min:1}),
+    check('categories').isLength({min:1})
   ],async (req, res)=>{
       //validacion de los datos de name por parte de express-validator
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
       }
-    const product= new Product({
-      name: req.body.name,
-      price: req.body.price,
-      categories: req.body.categories
-    })
+      const product= new Product({
+        name: req.body.name,
+        price: req.body.price,
+        categories: req.body.categories
+      })
 
     const result= await product.save()
-    res.status(201).send(product);//devolviendo el elemento nuevo que se creo con status 201 ya que es el resultado de solicitud metodo post
+    res.status(201).send(result);//devolviendo el elemento nuevo que se creo con status 201 ya que es el resultado de solicitud metodo post
   })
   
     
