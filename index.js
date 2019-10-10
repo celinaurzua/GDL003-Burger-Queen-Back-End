fa38e48db0a812a88efd59b99b8ef93cf05b2f92
celinaurzua <celina.urzua@gmail.com>
2019-10-09 19:10:25 -0500

    changed status 204 to 200 in put metohd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 27017
 app.listen(port, console.log('Escuchando en puerto: '+port))
 
 
-mongoose.connect('mongodb+srv://tagleurzua:mothersofcode@ciboullet-fc4ex.gcp.mongodb.net/cafedb', {useNewUrlParser: true, useFindAndModify:false})
+mongoose.connect('mongodb+srv://tagleurzua:mothersofcode@ciboullet-fc4ex.gcp.mongodb.net/cafedb', {useNewUrlParser: true, useUnifiedTopology:true})
   .then(resp=>console.log('Conectando correctamente a MongoDB'))
   .catch(err => console.log('Error al conectarse a MongoDB'))
 
